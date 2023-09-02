f59c8e5a26b0bba55785e66e0424b72d8620f15c
Daniel T Mpofu <danielmpofu@hotmail.com>
2023-09-02 15:48:11 +0200

    satsurday

--- src/Services/Discount/DiscountApi/Entities/Coupon.cs
+++ src/Services/Discount/DiscountApi/Entities/Coupon.cs
@@ -5,7 +5,6 @@
         public int Id { get; set; }
         public string ProductName { get; set; } 
         public string Description { get; set; }
-       // public string ProductCode { get; set; }
         public int Amount { get; set; }
     }
 }
